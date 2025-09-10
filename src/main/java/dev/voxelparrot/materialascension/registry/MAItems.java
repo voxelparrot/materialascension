f2fbdc7cf8246d89b4516b085583f064559aaa94
voxelparrot <voxelparrot@gmail.com>
2025-09-09 22:25:22 -0700

    cogatn't

--- src/main/java/dev/voxelparrot/materialascension/registry/MAItems.java
+++ src/main/java/dev/voxelparrot/materialascension/registry/MAItems.java
@@ -182,7 +182,7 @@ public class MAItems {
     items.add(new ItemInfo("handle", "nyxium_tool_handle", BasicComponentItem::new));
     items.add(new ItemInfo("dust", "ussingite_dust", BasicComponentItem::new));
 
-    items.add(new ItemInfo("misc", "cognata_icon", BasicComponentNonStackableItem::new));
+    //items.add(new ItemInfo("misc", "cognata_icon", BasicComponentNonStackableItem::new));
 
 
 
